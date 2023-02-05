0a232a6978bba815b5fed752fcca521842fd6456
etubom <way2young4u@hotmail.com>
2023-02-05 23:34:53 +0000

    chapter three and my character updated

--- pyCharacter.py
+++ pyCharacter.py
@@ -9,9 +9,17 @@ character_dob=date(1960, 11, 11).strftime('%d/%b/%Y')
 print("Character dob is",character_dob)
 character_net_worth = '{0:.2f}'.format(50000)
 print("Their net worth is ", '$'+ character_net_worth)
-character_super_powers=['reading', 'running', 'dancing']
+character_super_powers = {'reading':81, 'running':55, 'dancing':60,'jumping':70,'invisibility':45}
 print("Current set of super powers",character_super_powers)
-user_super_power_input=list(input("What is your favourite super power? : ").split())
-updated_super_powers = set(character_super_powers + user_super_power_input)
-print(updated_super_powers)
+user_super_power_input= input("What is your favourite super power? : ").lower()
+user_input_value = character_super_powers[user_super_power_input]
+print(user_input_value)
+if user_input_value < 80 and user_input_value > 50:
+    print("COOL")
+if user_input_value > 80:
+    print("SUPER COOL")
+
+character_wallet = {'dollars':'${:,.2f}'.format(170),'pounds':'£{:,.2f}'.format(50),'euros':'€{:,.2f}'.format(38)}
+print(character_wallet.get('euros'))
+
 
