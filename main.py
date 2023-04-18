91f43b1bc31a319e091448fa1a3c68cee3617ae7
etubom <way2young4u@hotmail.com>
2023-04-19 00:00:02 +0100

    chapter ten

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ character_super_powers: Dict[str, int] = {
 }
 character_wallet: Dict[str, float] = {"dollars": 170.0, "pounds": 50.0, "euros": 38.0}
 
-print(py_character.super_powers_at_or_above_given_number(character_super_powers, 70))
-print(py_character.spend_money(character_wallet, "dollars", 38))
+
+
+def main():
+    print(py_character.super_powers_at_or_above_given_number(character_super_powers, 70))
+    print(py_character.spend_money(character_wallet, "dollars", 38))
+
+
+if __name__ == "__main__":
+    main()
